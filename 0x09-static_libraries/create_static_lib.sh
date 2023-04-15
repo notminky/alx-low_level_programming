600037d8a2008bc9b6202586c1858ae69be89f7c
notminky <125456027+notminky@users.noreply.github.com>
2023-04-15 16:25:05 +0200

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-
-# Compile all .c files in the current directory into .o files
 gcc -Wall -pedantic -Werror -Wextra -c *.c
-
-# Create the static library liball.a from the .o files
 ar rcs liball.a *.o
-
-# Clean up by removing the .o files
 rm *.o
-
 echo "Static library liball.a has been created successfully."
