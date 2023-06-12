44475d81a3c2cc8f9042f739d7b54806496084fa
notminky <125456027+notminky@users.noreply.github.com>
2023-06-12 20:40:08 +0300

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
-int main(int argc, char **argv)
+
+int main(int argc, char *argv[])
 {
-    if (argc < 1)
-        exit(EXIT_FAILURE);
-    printf("%s\n", argv[0]);
-    return EXIT_SUCCESS;
+    if (argc > 0)
+    {
+        printf("%s\n", argv[0]);
+    }
+    return 0;
 }
