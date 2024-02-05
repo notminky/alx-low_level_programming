5c6a0ee598f1faca036fea5fde250b68f2754094
mohamed <eng.mobassem@outlook.com>
2024-02-05 10:41:51 -0800

    created 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+	for (letter = 'a'; letter =< 'z'; letter++)
+		putchar(letter);
+	for (letter = 'A'; letter =< 'Z'; letter++)
+		putchar(letter);
+
+	putchar(letter);
+
+	return(0);
+}
