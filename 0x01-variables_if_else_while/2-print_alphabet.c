cda2d08b146660db7d7db894ed5b142ea84879b3
Bassem <125456027+notminky@users.noreply.github.com>
2024-02-05 20:27:52 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,9 +2,12 @@
 /**main project on alphabet*/
 int main(void)
 {
-    char letter;
-    for (letter = 'a'; letter <= 'z'; letter++)
-        putchar(letter);
-    putchar('\n');
-    return (0);
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
 }
